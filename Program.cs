96970cfa1a0c82d78808fa0fdd04a5b4188e4cc2
pc <pc@DESKTOP-M7QJQNL>
2024-08-18 09:02:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+List<string> isimler = new List<string>();
+
+isimler.Add("Bülent Ersoy");
+isimler.Add("Ajda Pekkan");
+isimler.Add("Hadise");
+isimler.Add("Hande Yener");
+isimler.Add("Tarkan");
+
+foreach (string item in isimler)
+{
+    Console.WriteLine(item);
+}
